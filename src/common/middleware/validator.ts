0187a6ec849bb42ea5e6acf80e8ac5fe40819184
k-s-dev <kushagra.singh@gmail.com>
2026-01-29 09:05:53 +0530

    validation middleware setup with trials

---
+++ src/common/middleware/validator.ts
@@ -0,0 +1,92 @@
+import {
+  type NextFunction,
+  type Request,
+  type RequestHandler,
+  type Response,
+} from "express";
+import * as v from "valibot";
+import { ApiValidationError } from "../definitions/error.js";
+
+type ValibotObjectSchema = v.ObjectSchema<
+  v.ObjectEntries,
+  v.ErrorMessage<v.ObjectIssue>
+>;
+
+export const validateBody = <GenericBodySchema extends ValibotObjectSchema>(
+  schema: GenericBodySchema,
+): RequestHandler<
+  Request["params"],
+  object,
+  v.InferOutput<GenericBodySchema>
+> => {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const result = v.safeParse(schema, req.body);
+
+    if (!result.success) {
+      throw new ApiValidationError({
+        path: req.path,
+        message: "Validation failed for request body.",
+        errors: v.flatten(result.issues),
+      });
+    }
+
+    req.body = result.output;
+
+    next();
+  };
+};
+
+export const validateParams = <GenericParamsSchema extends ValibotObjectSchema>(
+  schema: GenericParamsSchema,
+): RequestHandler<Request["params"] & v.InferOutput<GenericParamsSchema>> => {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const result = v.safeParse(schema, req.params);
+
+    if (!result.success) {
+      throw new ApiValidationError({
+        path: req.path,
+        message: "Validation failed for request params.",
+        errors: v.flatten(result.issues),
+      });
+    }
+
+    (req as Request<v.InferOutput<GenericParamsSchema>>).params = result.output;
+
+    Object.keys(result.output).forEach((k) => {
+      if (!Object.keys(req.params).includes(k)) {
+        throw new ApiValidationError({
+          path: req.path,
+          message: "Validation failed for request params.",
+        });
+      }
+    });
+
+    next();
+  };
+};
+
+export const validatedQuery = <GenericQuerySchema extends ValibotObjectSchema>(
+  schema: GenericQuerySchema,
+): RequestHandler<
+  Request["params"],
+  object,
+  object,
+  Request["query"] & v.InferOutput<GenericQuerySchema>
+> => {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const result = v.safeParse(schema, req.query);
+
+    if (!result.success) {
+      throw new ApiValidationError({
+        path: req.path,
+        message: "Validation failed for request query.",
+      });
+    }
+
+    (
+      req as Request<object, object, object, v.InferOutput<GenericQuerySchema>>
+    ).query = result.output;
+
+    next();
+  };
+};
--- src/modules/api/routes.ts
+++ src/modules/api/routes.ts
@@ -1,4 +1,7 @@
+import { validateBody, validateParams } from "@/common/middleware/validator.js";
+import { logger } from "@/common/services/logger.js";
 import { Router } from "express";
+import * as v from "valibot";
 
 export const apiRouter = Router();
 
@@ -6,5 +9,22 @@ apiRouter.get("/", async (_req, res) => {
   res.json("Welcome to express app: API.");
 });
 
+const idSchema = v.pipe(v.string(), v.toNumber());
+const paramsSchema = v.object(
+  {
+    id: idSchema,
+  },
+  "Id is required.",
+);
+const bodySchema = paramsSchema;
 
-
+apiRouter.get(
+  "/:id",
+  validateParams(paramsSchema),
+  validateBody(bodySchema),
+  (req, res) => {
+    logger.info(req.params.id);
+    logger.info(req.body.id);
+    res.json("Welcome to express app: API.");
+  },
+);
