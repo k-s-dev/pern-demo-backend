b441794cd1d80346d87457d08af12108b2891f46
k-s-dev <kushagra.singh@gmail.com>
2026-01-29 09:25:41 +0530

    validation: cleanup trials

--- src/modules/api/routes.ts
+++ src/modules/api/routes.ts
@@ -1,7 +1,4 @@
-import { validateBody, validateParams } from "@/common/middleware/validator.js";
-import { logger } from "@/common/services/logger.js";
 import { Router } from "express";
-import * as v from "valibot";
 
 export const apiRouter = Router();
 
@@ -9,22 +6,3 @@ apiRouter.get("/", async (_req, res) => {
   res.json("Welcome to express app: API.");
 });
 
-const idSchema = v.pipe(v.string(), v.toNumber());
-const paramsSchema = v.object(
-  {
-    id: idSchema,
-  },
-  "Id is required.",
-);
-const bodySchema = paramsSchema;
-
-apiRouter.get(
-  "/:id",
-  validateParams(paramsSchema),
-  validateBody(bodySchema),
-  (req, res) => {
-    logger.info(req.params.id);
-    logger.info(req.body.id);
-    res.json("Welcome to express app: API.");
-  },
-);
