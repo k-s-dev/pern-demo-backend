9cd46343c24d862aadfc2a8a00de42969d7d6fcb
k-s-dev <kushagra.singh@gmail.com>
2026-02-19 19:39:36 +0530

    new route: auth api test

--- src/modules/nextDemo/api/lib/routes.ts
+++ src/modules/nextDemo/api/lib/routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import { isAuthenticated } from "../../lib/auth/authentication.middleware.js";
 
 export const nextDemoApiRouter = Router();
 
 nextDemoApiRouter.get("/", (_req, res) => {
   res.json("Welcome to express app:nextDemo:api.");
+});
+
+nextDemoApiRouter.get("/test-auth/", isAuthenticated, (_req, res) => {
+  res.json({
+    message: "Test protected endpoint.",
+    user: res.locals.user,
+  });
 });
